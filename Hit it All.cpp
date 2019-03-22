2f91d6e467f670262152f62e3746391397274d9c
shinjefD <47912325+shinjefD@users.noreply.github.com>
2019-03-23 00:01:03 +0800

    Update Hit it All.cpp

--- Hit it All.cpp
+++ Hit it All.cpp
@@ -8,27 +8,9 @@
 #include <conio.h>
 #include <math.h> //for sin in 
 #include "console.h"
-char* BUBBLE = "*";
-char* AIM = "o";
-int colors[WIDTH][HEIGHT];
-int palette[5] = {TRED, TBLUE, TGREEN, TYELLOW, TBLUEGREEN};
-float velocity[2]; 
-float resVelocity = 2;
-float angle = 90;
-int bubbleCoord[2] = {25,40};
-int ballColor = TRED;
-void controls();
-int X_COORD = 0;
-int Y_COORD = 1;
-float fakeVelocity[2];
-float fakebubbleCoord[2];
-float fakeAngle;
-int score = 0;
-int tries = 0;
-bool isHit = false;
-
-
-//#include "allGlobalVariables.h"
+char* BUBBLE = 	'*';
+char* AIM = 'o';
+#include "compiledVariables.h"
 #include "display.h"
 #include "manuever.h"
 #include "checker.h"
@@ -37,7 +19,7 @@ int main(){
 	srand(time(NULL));
 	//setScreenSize(WIDTH, HEIGHT);
 	setWindowSize(WIDTH,HEIGHT);
-	//wall();
+	wall();
 	int counter = 0;
 	int y = rand() % (HEIGHT - 20);
 	int x = rand() % WIDTH;
@@ -53,5 +35,4 @@ int main(){
 	}
 	return 0;
 }
-
 
