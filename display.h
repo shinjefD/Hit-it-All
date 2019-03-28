ed155d7d99ba5313f52356439b0808ef78b73872
shinjefD <47912325+shinjefD@users.noreply.github.com>
2019-03-28 11:10:38 +0800

    Update display.h

--- display.h
+++ display.h
@@ -1,6 +1,11 @@
-/*void wall(){
-
-}*/
+void wall(){
+	for(int i = 0; i != 50; i++){
+		putxy(1, i, TRED, WALL);
+	}
+	for(int i = 0; i != 50; i++){
+		putxy(49, i, TBLUE, WALL);
+	}
+}
 
 void generateBubbles(int& counter, int &x, int &y){
 	if (counter >= BUBBLECOUNT) return;
@@ -13,10 +18,10 @@ void generateBubbles(int& counter, int &x, int &y){
 		generateBubbles(counter, x, y);
 		return;
 	}
-	counter++;
 	int newX = x + stepX;
 	int newY = y + stepY;
-	if (newY > YLIMIT) generateBubbles(counter, x, y);
+	if (newX == 1 || newY == 49 || newY > YLIMIT) generateBubbles(counter, x, y);
+	counter++;
 	generateBubbles(counter, newX, newY);
 }
 
@@ -25,11 +30,17 @@ void ball(){
 		//create new player bubble
 		//random code shit
 	//	isHit = false;
-	//}
+	//
 	putxy(bubbleCoord[X_COORD], bubbleCoord[Y_COORD], ballColor, " ");
 	bubbleCoord[X_COORD] += velocity[X_COORD];
 	bubbleCoord[Y_COORD] -= velocity[Y_COORD];
 	putxy(bubbleCoord[X_COORD], bubbleCoord[Y_COORD], ballColor, BUBBLE);
+	int checkWallCollision[2];
+	checkWallCollision[X_COORD] = bubbleCoord[X_COORD] + velocity[X_COORD];
+	checkWallCollision[Y_COORD] = bubbleCoord[Y_COORD] - velocity[X_COORD];
+	if (getConsoleChar(checkWallCollision[X_COORD], checkWallCollision[Y_COORD]) == WALL[0])
+		velocity[X_COORD] = -velocity[X_COORD];
+		//bubbleCoord[X_COORD] = -bubbleCoord[X_COORD];
 }
 
 
@@ -38,22 +49,20 @@ void target(){
 		fakeVelocity[X_COORD] = resVelocity * cos(fakeAngle * (M_PI/180));
 		fakeVelocity[Y_COORD] = resVelocity * sin(fakeAngle * (M_PI/180));
 		fakebubbleCoord[X_COORD] = bubbleCoord[X_COORD];
-		fakebubbleCoord[Y_COORD] =  bubbleCoord[Y_COORD];
-		
-		for (int i = 0; i != 1; i++){
-			fakebubbleCoord[X_COORD] -= 1 * fakeVelocity[X_COORD];
-			fakebubbleCoord[Y_COORD] += 1 * fakeVelocity[Y_COORD];
-			char *empty = " ";
+		fakebubbleCoord[Y_COORD] = bubbleCoord[Y_COORD];
+		for (int i = 0; i != 5; i++){
+			fakebubbleCoord[X_COORD] += fakeVelocity[X_COORD];
+			fakebubbleCoord[Y_COORD] -= fakeVelocity[Y_COORD];
+			char* empty = " ";
 			putxy(fakebubbleCoord[X_COORD], fakebubbleCoord[Y_COORD], ballColor, empty);
 		}
 
 		fakebubbleCoord[X_COORD] = bubbleCoord[X_COORD];
-		fakebubbleCoord[Y_COORD] =  bubbleCoord[Y_COORD];
-		for (int i = 0; i != 1; i++){
-			fakebubbleCoord[X_COORD] += 2 * fakeVelocity[X_COORD];
-			fakebubbleCoord[Y_COORD] -= 2 * fakeVelocity[Y_COORD];
+		fakebubbleCoord[Y_COORD] = bubbleCoord[Y_COORD];
+		for (int i = 0; i != 5; i++){
+			fakebubbleCoord[X_COORD] += fakeVelocity[X_COORD];
+			fakebubbleCoord[Y_COORD] -= fakeVelocity[Y_COORD];
 			putxy(fakebubbleCoord[X_COORD], fakebubbleCoord[Y_COORD], ballColor, AIM);
 		}
 	}
 }
-
